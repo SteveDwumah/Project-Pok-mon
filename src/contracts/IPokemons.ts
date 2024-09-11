66a748b8e9e2f9ed98f457c7b6c6d4cf5a0f02a3
SteveDwumah <steve.dwmh@outlook.de>
2024-09-11 16:02:01 +0200

    Pokemon Project

---
+++ src/contracts/IPokemons.ts
@@ -0,0 +1,16 @@
+export interface IPokemons {
+    count: number;
+    next: string;
+    previous: null;
+    results: IPokemonResult[];
+}
+
+export interface IPokemonResult {
+    name: string;
+    url: string;
+}
+
+
+export interface IPokemonType {
+    pokemon: IPokemonResult
+}
